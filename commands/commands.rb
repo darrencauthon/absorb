261e04c2d0014bd8d0056660b6e4d68df9f09636
Darren Cauthon <darren@cauthon.com>
2013-03-10 22:14:39 -0500

    Refactor.

--- commands/commands.rb
+++ commands/commands.rb
@@ -1,13 +1,25 @@
 module Commands
-  def self.the_appropriate_command
-    name = ARGV[0]
 
-    command_type = Commands::Base.commands.select { |x| x.command_name == name }.first || Default
-    options = command_type.option_parser.options
-    arguments = ARGV
-    arguments.shift unless command_type == Default
+  def self.the_appropriate_command
     puts arguments.inspect
     puts options.inspect
     command_type.new arguments, options
+  end
+
+  private
+
+  def self.command_type
+    @command_type ||= Commands::Base.commands.select { |x| x.command_name == ARGV[0] }.first || Default
+  end
+
+  def self.options
+    @options ||= command_type.option_parser.options
+  end
+
+  def self.arguments
+    return @arguments if @arguments
+    @arguments = ARGV
+    @arguments.shift unless command_type == Default
+    @arguments
   end
 end
