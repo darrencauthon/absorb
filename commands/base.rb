eb69312a150ef65d15f57fc2801c719e7778dfee
Darren Cauthon <darren@cauthon.com>
2013-03-10 19:29:30 -0500

    More reorganization.

--- commands/base.rb
+++ commands/base.rb
@@ -8,20 +8,6 @@ module Commands
         @commands << command
       end
 
-      def default_option_parser
-        OptionParser.new do |opt|
-          opt.banner = 'Usage: absorb COMMAND [OPTIONS]'
-          opt.separator ''
-          opt.separator 'Commands'
-          opt.separator '     file: Absorb a file.'
-          opt.separator ''
-          opt.separator 'Options'
-
-          opt.on('-h','--help','help') do
-            puts option_parser
-          end
-        end
-      end
     end
   end
 end
