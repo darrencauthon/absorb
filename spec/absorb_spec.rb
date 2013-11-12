dbe2ecd516fcb4fc763b6d0c56c0ce7a2f1ebdf3
Darren Cauthon <darren@cauthon.com>
2013-11-11 21:36:23 -0600

    Get the first working test.

---
+++ spec/absorb_spec.rb
@@ -0,0 +1,8 @@
+require_relative 'spec_helper'
+
+describe Absorb do
+  it "should exist" do
+    true.must_equal true
+  end
+end
+
---
+++ spec/spec_helper.rb
@@ -0,0 +1,4 @@
+require_relative '../lib/absorb'
+require 'minitest/autorun'
+require 'minitest/spec'
+require 'minitest/pride'
