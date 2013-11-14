5d3227a2ea4fa9fbefd796f75d29b24bf0f2f3ca
Darren Cauthon <darren@cauthon.com>
2013-11-13 23:12:48 -0600

    First working spec.

--- features/steps/absorb_files.rb
+++ features/steps/absorb_files.rb
@@ -2,22 +2,17 @@ require_relative 'common'
 
 class Spinach::Features::AbsorbFiles < Spinach::FeatureSteps
 
-  before { setup_s3 }
-
-  attr_accessor :file
-
   step 'I have a file' do
-    file = 'file.txt'
-    create_a_file file
+    @file = 'file.txt'
+    create_a_file @file
   end
 
   step 'I absorb the file' do
-    Absorb.file test_file(file)
+    Absorb.file test_file(@file)
   end
 
   step 'the file should be uploaded to S3' do
-    #AWS::S3::S3Object.store(file, open(test_file(file)), bucket_name)
-    bucket[file].nil?.must_equal false
+    bucket[@file].nil?.must_equal false
   end
 
   def create_a_file file, content = 'x'
@@ -34,23 +29,5 @@ class Spinach::Features::AbsorbFiles < Spinach::FeatureSteps
 
   def bucket_name
     ENV['BUCKET']
-  end
-
-  def setup_s3
-
-    AWS::S3::Base.establish_connection!(
-      :access_key_id     => ENV['ACCESS_KEY_ID'],
-      :secret_access_key => ENV['SECRET_ACCESS_KEY']
-    )
-
-    begin
-      AWS::S3::Bucket.delete(bucket_name, force: true)
-    rescue
-    end
-
-    begin
-      AWS::S3::Bucket.create(bucket_name)
-    rescue
-    end
   end
 end
--- lib/absorb.rb
+++ lib/absorb.rb
@@ -2,6 +2,27 @@ require 'aws/s3'
 require "absorb/version"
 
 module Absorb
+  def self.bucket_name
+    ENV['BUCKET']
+  end
+
   def self.file file
+    AWS::S3::Base.establish_connection!(
+      :access_key_id     => ENV['ACCESS_KEY_ID'],
+      :secret_access_key => ENV['SECRET_ACCESS_KEY']
+    )
+
+    begin
+      AWS::S3::Bucket.delete(bucket_name, force: true)
+    rescue
+    end
+
+    begin
+      AWS::S3::Bucket.create(bucket_name)
+    rescue
+    end
+
+    name = file.split('/')[-1]
+    AWS::S3::S3Object.store(name, open(file), bucket_name)
   end
 end
