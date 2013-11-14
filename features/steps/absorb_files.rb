ad89c42262d1d027883b425cd63dbe3bb574a792
Darren Cauthon <darren@cauthon.com>
2013-11-13 22:11:53 -0600

    Actually create a file.

--- features/steps/absorb_files.rb
+++ features/steps/absorb_files.rb
@@ -3,6 +3,7 @@ require_relative 'common'
 class Spinach::Features::AbsorbFiles < Spinach::FeatureSteps
   step 'I have a file' do
     @file = 'file.txt'
+    create_a_file @file
   end
 
   step 'I absorb the file' do
@@ -11,5 +12,9 @@ class Spinach::Features::AbsorbFiles < Spinach::FeatureSteps
 
   step 'the file should be uploaded to S3' do
     pending 'start by determining how to judge this'
+  end
+
+  def create_a_file file, content = 'x'
+    File.open('temp/file.txt', 'w') { |file| file.write(content) }
   end
 end
