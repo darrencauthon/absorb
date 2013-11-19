3eeeda242a9a165db4a624f8b6718202ddd3a5d9
Darren Cauthon <darren@cauthon.com>
2013-11-18 22:27:00 -0600

    Move the dynamodb database setup from the specs to real code.

--- features/steps/absorb_files.rb
+++ features/steps/absorb_files.rb
@@ -9,16 +9,6 @@ class Spinach::Features::AbsorbFiles < Spinach::FeatureSteps
     @guid = 'abc'
     Absorb::Guid.stubs(:generate).returns 'abc'
 
-    Dynamoid.configure do |config|
-      config.adapter = 'aws_sdk' # This adapter establishes a connection to the DynamoDB servers using Amazon's own AWS gem.
-      config.namespace = "absorb_test" # To namespace tables created by Dynamoid from other tables you might have.
-      config.warn_on_scan = true # Output a warning to the logger when you perform a scan rather than a query on a table.
-      config.partitioning = true # Spread writes randomly across the database. See "partitioning" below for more.
-      config.partition_size = 200  # Determine the key space size that writes are randomly spread across.
-      config.read_capacity = 100 # Read capacity for your tables
-      config.write_capacity = 20 # Write capacity for your tables
-    end
-
     Upload.all.each do |upload|
       upload.delete
     end
--- lib/absorb/amazon.rb
+++ lib/absorb/amazon.rb
@@ -5,6 +5,17 @@ module Amazon
     AWS.config(access_key_id:       Absorb.settings[:access_key_id],
                secret_access_key:   Absorb.settings[:secret_access_key],
                dynamo_db_endpoint: 'dynamodb.us-east-1.amazonaws.com')
+
+    Dynamoid.configure do |config|
+      config.adapter = 'aws_sdk' # This adapter establishes a connection to the DynamoDB servers using Amazon's own AWS gem.
+      config.namespace = "absorb_test" # To namespace tables created by Dynamoid from other tables you might have.
+      config.warn_on_scan = true # Output a warning to the logger when you perform a scan rather than a query on a table.
+      config.partitioning = true # Spread writes randomly across the database. See "partitioning" below for more.
+      config.partition_size = 200  # Determine the key space size that writes are randomly spread across.
+      config.read_capacity = 100 # Read capacity for your tables
+      config.write_capacity = 20 # Write capacity for your tables
+    end
+
     @started_up = true
   end
 
