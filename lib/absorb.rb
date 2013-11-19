d689fa44fe53aecac270d93f4b078026097db3e2
Darren Cauthon <darren@cauthon.com>
2013-11-18 22:30:50 -0600

    Bind the upload table name to config.

--- lib/absorb.rb
+++ lib/absorb.rb
@@ -10,6 +10,7 @@ module Absorb
       bucket_name:       ENV['BUCKET_NAME'],
       access_key_id:     ENV['ACCESS_KEY_ID'],
       secret_access_key: ENV['SECRET_ACCESS_KEY'],
+      dynamodb_upload:   ENV['DYNAMODB_UPLOAD'],
     }
   end
 
--- lib/absorb/amazon.rb
+++ lib/absorb/amazon.rb
@@ -8,7 +8,7 @@ module Amazon
 
     Dynamoid.configure do |config|
       config.adapter = 'aws_sdk' # This adapter establishes a connection to the DynamoDB servers using Amazon's own AWS gem.
-      config.namespace = "absorb_test" # To namespace tables created by Dynamoid from other tables you might have.
+      config.namespace = Absorb.settings[:dynamodb_upload] # To namespace tables created by Dynamoid from other tables you might have.
       config.warn_on_scan = true # Output a warning to the logger when you perform a scan rather than a query on a table.
       config.partitioning = true # Spread writes randomly across the database. See "partitioning" below for more.
       config.partition_size = 200  # Determine the key space size that writes are randomly spread across.
