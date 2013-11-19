a92b5b8dd9b88fa6e3e8c9063321b7319ec5a5d3
Darren Cauthon <darren@cauthon.com>
2013-11-18 23:30:47 -0600

    Refactor.

--- lib/absorb/absorber.rb
+++ lib/absorb/absorber.rb
@@ -1,17 +1,19 @@
 module Absorb
   class Absorber
     def absorb files
-
       upload = Absorb::Upload.create(uuid: Absorb::Guid.generate)
+      files.each { |f| add f, upload }
+    end
 
-      files.each do |file|
-        name = file.split('/')[-1]
+    private
 
-        Absorb::File.create(uuid: upload.uuid, name: name)
+    def add file, upload
+      name = file.split('/')[-1]
 
-        s3 = Absorb::AmazonS3.new
-        s3.store_file file, "#{upload.uuid}/#{name}"
-      end
+      Absorb::File.create(uuid: upload.uuid, name: name)
+
+      s3 = Absorb::AmazonS3.new
+      s3.store_file file, "#{upload.uuid}/#{name}"
     end
   end
 end
