2473170b4300591a9eca550ee4edb8624d58cc30
Darren Cauthon <darren@cauthon.com>
2013-11-23 17:27:35 -0600

    Rename method.

--- lib/absorb/absorber.rb
+++ lib/absorb/absorber.rb
@@ -10,16 +10,16 @@ module Absorb
     private
 
     def add file, upload
-      file_name = get_the_file_name_from file
-      Absorb::File.create(uuid: upload.uuid, name: file_name)
-      s3.store_file file, "#{upload.uuid}/#{file_name}"
+      relative_file = get_the_relative_file_from file
+      Absorb::File.create(uuid: upload.uuid, name: relative_file)
+      s3.store_file file, "#{upload.uuid}/#{relative_file}"
     end
 
     def s3
       Absorb::AmazonS3.new
     end
 
-    def get_the_file_name_from file
+    def get_the_relative_file_from file
       segments = file.split('/')
       segments.shift if segments.count > 1
       segments.join('/')
