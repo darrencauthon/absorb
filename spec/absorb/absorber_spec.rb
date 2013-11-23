5830e6f69df24a6dbb8564d96a70b4239ec5f8dc
Darren Cauthon <darren@cauthon.com>
2013-11-23 16:43:04 -0600

    Add unit tests on Absorb.

--- spec/absorb/absorber_spec.rb
+++ spec/absorb/absorber_spec.rb
@@ -1,4 +1,29 @@
 require_relative '../spec_helper'
 
 describe Absorb::Absorber do
+
+  describe "files" do
+
+    let(:files)    { Object.new }
+    let(:absorber) { Object.new }
+
+    before do
+      Amazon.expects(:startup)
+
+      Absorb::Absorber.stubs(:new).returns absorber
+      absorber.expects(:absorb).with files
+
+      Absorb.files files
+    end
+
+    it "should start up Amazon" do
+      # expectation set above
+    end
+
+    it "should create an absorber and pass along the files" do
+      # expectation set above
+    end
+
+  end
+
 end
