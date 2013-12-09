b20fc51e0b2626bae67d64863f8e0d880c8f9de2
Darren Cauthon <darren@cauthon.com>
2013-12-08 19:08:21 -0600

    Refactor.

--- lib/absorb/record_the_file_in_dynamo_worker.rb
+++ lib/absorb/record_the_file_in_dynamo_worker.rb
@@ -7,18 +7,35 @@ module Absorb
     end
 
     def process
-      file = effort.data['file']
-      package = Absorb::Package.find(effort.data['package_id'])
-      relative_file = get_the_relative_file_from effort.data['file']
-      Absorb::File.create(uuid: package.uuid, name: relative_file)
-      effort.data['package_uuid'] = package.uuid
+      create_the_file
+      save_important_details_for_later
+    end
+
+    def save_important_details_for_later
+      effort.data['package_uuid']  = package.uuid
       effort.data['relative_file'] = relative_file
+    end
+
+    def create_the_file
+      Absorb::File.create(uuid: package.uuid, name: relative_file)
     end
 
     def get_the_relative_file_from file
       segments = file.split('/')
       segments.shift if segments.count > 1
       segments.join('/')
+    end
+
+    def file
+      effort.data['file']
+    end
+
+    def relative_file
+      get_the_relative_file_from file
+    end
+
+    def package
+      @package ||= Absorb::Package.find(effort.data['package_id'])
     end
 
   end
