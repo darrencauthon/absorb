aef2ee72a349c78b1f050f6dff7451604119fbaf
Darren Cauthon <darren@cauthon.com>
2013-12-29 23:20:22 -0600

    Replace the explicit list of things to do for one based on conventions.

--- lib/absorb/absorber.rb
+++ lib/absorb/absorber.rb
@@ -1,6 +1,18 @@
 module Absorb
 
+  class ::Seam::Worker
+    def self.inherited worker
+      step = Absorb::Helpers.underscore(worker.to_s).gsub('absorb/', '').to_sym
+      Absorb::Absorber.things_to_do ||= {}
+      Absorb::Absorber.things_to_do[step] = worker
+    end
+  end
+
   class Absorber
+
+    class << self
+      attr_accessor :things_to_do
+    end
 
     def self.package_flow_for files
       flow = Seam::Flow.new
@@ -30,6 +42,7 @@ module Absorb
     end
 
     def complete_the_work
+      things_to_do = self.class.things_to_do
       while steps_to_run.count > 0
         steps_to_run.each { |s| things_to_do[s.to_sym].new.execute_all }
       end
@@ -38,17 +51,6 @@ module Absorb
     def steps_to_run
       Seam.steps_to_run
     end
-
-    def things_to_do
-      @things_to_do ||= { 
-                          create_a_package:                          CreateAPackage,
-                          add_a_file_to_the_package:                 AddAFileToThePackage,
-                          upload_file_to_s3_if_necessary:            UploadFileToS3IfNecessary,
-                          determine_this_files_status_in_the_system: DetermineThisFilesStatusInTheSystem,
-                          record_the_file_in_dynamo:                 RecordTheFileInDynamo
-                        }
-    end
-
 
   end
 
