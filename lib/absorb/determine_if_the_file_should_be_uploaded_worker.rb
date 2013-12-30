8c57a5a3cfbc24a9b053710e831dad53fc1be802
Darren Cauthon <darren@cauthon.com>
2013-12-29 20:47:29 -0600

    Refactor.

--- lib/absorb/determine_if_the_file_should_be_uploaded_worker.rb
+++ lib/absorb/determine_if_the_file_should_be_uploaded_worker.rb
@@ -7,15 +7,29 @@ module Absorb
     end
 
     def process
-      file          = Absorb::File.find effort.data['the_file_id']
-      similar_files = Absorb::File.where(md5: effort.data['md5'])
-      similar_files = similar_files.select { |x| x.id != file.id }
-      if similar_files.count == 0
-        file.storage_id = file.uuid
-      else
-        file.storage_id = similar_files.first.storage_id
-      end
+      file = retrieve_this_file
+      file.storage_id = the_appropriate_file_storage_id_for file
       file.save
+    end
+
+    private
+
+    def the_appropriate_file_storage_id_for file
+      similar_files = find_similar_files_to file
+      if similar_files.count > 0
+        similar_files.first.storage_id
+      else
+        file.uuid
+      end
+    end
+
+    def retrieve_this_file
+      Absorb::File.find effort.data['the_file_id']
+    end
+
+    def find_similar_files_to file
+      similar_files = Absorb::File.where(md5: effort.data['md5'])
+      similar_files.select { |x| x.id != file.id }
     end
 
   end
