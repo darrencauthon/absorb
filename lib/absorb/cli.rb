d5ad46235269f42959086a084ab76721c24a3530
Darren Cauthon <darren@cauthon.com>
2015-11-26 21:48:26 -0600

    Refactor.

--- lib/absorb/cli.rb
+++ lib/absorb/cli.rb
@@ -3,23 +3,9 @@ module Absorb
   module CLI
 
     def self.run argv
+      plan = execution_plan_from argv
 
-      files = if File.exist?(argv[0]) && File.directory?(argv[0]) == false
-                [File.expand_path(File.dirname(argv[0]) + '/' + argv[0])]
-              else
-                all_files_in(argv[0]).map { |x| File.expand_path(File.dirname(x) + '/' + x.split('/')[-1]) }
-              end
-
-      home = if File.exist?(argv[0]) && File.directory?(argv[0]) == false
-               File.expand_path(File.dirname(argv[0]))
-             else
-               File.expand_path(argv[0])
-             end
-
-      plan = Absorb.checking_script_for( { home: home, files: files } )
-      workflow = Onus::Workflow.new plan
-
-      workflow.execute
+      Onus::Workflow.new(plan).execute
     end
 
     class << self
@@ -29,6 +15,29 @@ module Absorb
       def all_files_in dir
         Dir[ File.join(dir, '**', '*') ].reject { |p| File.directory? p }
       end 
+
+      def execution_plan_from argv
+        files = requested_files_from argv
+        home  = home_folder_from argv
+
+        Absorb.checking_script_for( { home: home, files: files } )
+      end
+
+      def requested_files_from argv
+        if File.exist?(argv[0]) && File.directory?(argv[0]) == false
+          [File.expand_path(File.dirname(argv[0]) + '/' + argv[0])]
+        else
+          all_files_in(argv[0]).map { |x| File.expand_path(File.dirname(x) + '/' + x.split('/')[-1]) }
+        end
+      end
+
+      def home_folder_from argv
+        if File.exist?(argv[0]) && File.directory?(argv[0]) == false
+          File.expand_path(File.dirname(argv[0]))
+        else
+          File.expand_path(argv[0])
+        end
+      end
 
     end
 
