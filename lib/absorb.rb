f44ecbe794c930e2b16836dacc699a5764013e36
Darren Cauthon <darren@cauthon.com>
2015-11-27 07:59:55 -0600

    Extract some methods.

--- lib/absorb.rb
+++ lib/absorb.rb
@@ -4,18 +4,32 @@ Dir[File.dirname(__FILE__) + '/absorb/*.rb'].each {|file| require file }
 module Absorb
 
   def self.checking_script_for options
-    commands = []
-    options[:files]
-      .map  { |f| "Plan to absorb #{f} if it has not been absorbed" }
-      .each { |c| commands << c }
+    commands = plan_to_absorb_every_file(options)
 
-    commands << "Create a new directory at #{options[:storage_folder]}"
+    commands << create_the_new_storage_folder(options)
 
-    options[:files]
-      .map  { |f| "Copy #{f} to #{options[:storage_folder]}#{f.sub(options[:home], '')} if the file should be absorbed" }
+    copy_the_files_to_the_storage_folder(options)
       .each { |c| commands << c }
 
     commands.join("\n")
+  end
+
+  class << self
+
+    def plan_to_absorb_every_file options
+      options[:files]
+        .map { |f| "Plan to absorb #{f} if it has not been absorbed" }
+    end
+
+    def create_the_new_storage_folder options
+      "Create a new directory at #{options[:storage_folder]}"
+    end
+
+    def copy_the_files_to_the_storage_folder options
+      options[:files]
+        .map  { |f| "Copy #{f} to #{options[:storage_folder]}#{f.sub(options[:home], '')} if the file should be absorbed" }
+    end
+
   end
 
 end
